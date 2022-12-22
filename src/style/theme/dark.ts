37abdf546e96e4e83d72690d78c18eaf3321bece
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-12-23 02:25:19 +0300

    restyle theme done

---
+++ src/style/theme/dark.ts
@@ -0,0 +1,53 @@
+import { createTheme } from "@shopify/restyle";
+import light, { Theme } from './light';
+import { StatusBarStyle } from 'react-native'
+
+const colorPalette = {
+	slate00: '#1b1c1d',
+	slate10: '#202225',
+	slate20: '#292c2f',
+	slate30: '#2e3235',
+	slate40: '#35393d',
+	slate100: '#767577',
+	slate900: '#dddddd',
+	blue70: '#2185d0'
+}
+
+const theme = createTheme<Theme>({
+	...light,
+	  colors: {
+    ...light.colors,
+    $primary: colorPalette.blue70,
+//    $secondary: colorPalette.slate00,
+    $windowBackground: colorPalette.slate10,
+    $background: colorPalette.slate10,
+    $foreground: colorPalette.slate900,
+//    $separator: colorPalette.slate100,
+    $navbarBackground: colorPalette.slate20,
+    $navbarBorderBottom: colorPalette.slate00,
+    $headerBarBackground: colorPalette.slate40,
+    $sidebarBackground: colorPalette.slate30,
+    $sidebarForeground: colorPalette.slate900,
+    $sidebarSeparator: colorPalette.slate900 + '20',
+    $fieldInputBackground: colorPalette.slate00,
+    $fieldInputPlaceholderTextColor: colorPalette.slate100
+  },
+  statusBar: {
+    barStyle: 'light-content' as StatusBarStyle
+  },
+  textVariants: {
+    ...light.textVariants
+  },
+  barVariants: {
+    headerBar: {
+      bg: '$headerBarBackground',
+      borderRadius: 'hg',
+ //     shadowColor: 'black',
+ //     shadowOffset: { width: 0, height: 2 },
+ //     shadowOpacity: 0.4,
+ //     shadowRadius: 8
+    }
+  }
+})
+
+export default theme;
---
+++ src/style/theme/light.ts
@@ -0,0 +1,87 @@
+import { createTheme } from "@shopify/restyle";
+import { StatusBarStyle } from "react-native";
+
+const colorPalette = {
+  white: 'white',
+  black: 'black',
+  red: 'red',
+  blue: 'blue',
+  yellow: 'yellow',
+  paper00: '#ffffff',
+  paper10: '#f5f5f4',
+  paper20: '#e6e6e6',
+  paper100: '#aeaeae',
+  paper300: '#767577',
+  paper900: '#202020',
+  blue70: '#2185d0',
+  navy20: '#171a21',
+  navy900: '#b9babc'
+}
+
+const theme = createTheme({
+	spacing: {
+		'0': 0,
+		xs: 4,
+		sm: 8,
+		md: 12,
+		lg: 16,
+		xl: 24,
+		xxl: 48,
+		hg: 128
+	},
+	breakpoints: {
+		phone: 0,
+		tablet: 768
+	},
+	colors: {
+		white: colorPalette.white,
+		black: colorPalette.black,
+		red: colorPalette.red,
+		blue: colorPalette.blue,
+		yellow: colorPalette.yellow,
+
+		$primary: colorPalette.blue70,
+		$windowBackground: '#f0f0f0',
+		$background: colorPalette.paper10,
+		$foreground: colorPalette.paper900,
+		$navbarBackground: colorPalette.paper10,
+		$navbarBorderBottom: colorPalette.paper100,
+		$sidebarBackground: colorPalette.navy20,
+		$sidebarForeground: colorPalette.navy900,
+		$sidebarSeparator: colorPalette.paper00 + '20',
+		$headerBarBackground: colorPalette.paper20,
+		$fieldInputBackground: colorPalette.paper00,
+		$fieldInputPlaceholderTextColor: colorPalette.paper300
+	},
+	borderRadii: {
+		xs: 4,
+		sm: 16,
+		md: 24,
+		lg: 64,
+		hg: 128
+	},
+	statusBar: {
+		barStyle: 'dark-content' as StatusBarStyle
+	},
+	textVariants: {
+		defaults: {
+		color: '$foreground',
+		fontSize: 16
+		},
+		sidebar: {
+		color: '$sidebarForeground'
+		},
+		navbar: {
+		fontSize: 20
+		}
+	},
+	barVariants: {
+		headerBar: {
+		bg: '$headerBarBackground',
+		borderRadius: 'hg'
+		}
+	}
+})
+
+export default theme;
+export type Theme = typeof theme;
